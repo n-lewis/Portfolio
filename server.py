92a9b987b1a5c6932a3bce41e382b4856d89c52a
Nate Lewis <nathaniel.lewis@wsu.edu>
2019-11-13 17:48:27 -0800

    Adds gitignore

--- server.py
+++ server.py
@@ -1,7 +1,28 @@
 from flask import Flask, render_template, request, flash, redirect
+from flask_mail import Mail, Message
 import os
 app = Flask(__name__)
 app.secret_key = os.urandom(12)
+
+mail_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": os.environ['EMAIL_USER'],
+    "MAIL_PASSWORD": os.environ['EMAIL_PASSWORD']
+}
+
+app.config.update(mail_settings)
+mail = Mail(app)
+
+
+def send(data):
+    msg = Message(subject=data['subject'],
+                  sender=app.config.get(""),
+                  recipients=[""],
+                  body=data['email'] + data['body'])
+    mail.send(msg)
 
 
 @app.route('/')
