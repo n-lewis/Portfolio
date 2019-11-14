d368fdb8dcce939ffbb6bd7d37a0503ca152f6c3
Nate Lewis <nathaniel.lewis@wsu.edu>
2019-11-13 17:28:23 -0800

    Adds all static personal information
    Currently projects and contact form are the last two things to implement. The concact form currently grabs the info and returns it to the server.py route /submit_form.

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect
+import os
 app = Flask(__name__)
+app.secret_key = os.urandom(12)
 
 
 @app.route('/')
@@ -7,6 +9,22 @@ def root():
     return render_template('index.html')
 
 
-@app.route('/help')
-def about():
-    return render_template('components.html')
+@app.route('/<page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submint_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        print(data)
+        if data['email'] and data['subject'] and data['message']:
+            flash('Form submitted! I will get back to you as soon as I can.')
+            return redirect('index.html')
+        else:
+            flash('Invalid submission')
+            return redirect('index.html')
+    else:
+        flash('Something went wrong with your submission')
+        return redirect('index.html')
